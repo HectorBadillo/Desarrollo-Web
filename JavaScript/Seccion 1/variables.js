2720b05679376235e3a00d8c408c05bca1b99839
Hector Badillo <dsm.badillo@gmail.com>
2024-02-29 13:41:42 -0600

    Nuevo

---
+++ JavaScript/Seccion 1/variables.js
@@ -0,0 +1,18 @@
+//Definicion de variables
+
+let nombre = 'Hector';
+console.log(nombre);
+
+//Forma antigua
+var nombre2 = 'Francisco';
+console.log(nombre2)
+
+//Modificar valor
+nombre = '19'
+console.log(nombre)
+
+//Constante
+const pais = 'Mexico'
+
+// pais = 'Peru' no se puede modificar
+console.log(pais)
